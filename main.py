9ffa6fa126c10cfc0681378f49ad2a75e90964c2
Khanh Tran <dkhanh1702@gmail.com>
2020-06-04 05:54:41 -0400

    Move the code finding engulfing structure to market_analysis.py

--- main.py
+++ main.py
@@ -30,41 +30,13 @@ def main(argv):
         md.market_data_get_current_price()
         access_twitter_bot()
         return 0
-
-    interval = int(argv[1])
-    limit = int(argv[2])
-    trend = str(argv[3])
-
-    if ((interval > 0 & interval <  720) |
-        (limit > 0 & limit < 10000) |
-        ((trend == "bullish") | (trend == "bearish"))):
-        market_data = md.market_data_query_kline(interval, limit)
-        engulfing = ma.market_analysis_find_engulfing_structure(market_data, trend)
-
-        # if bearish: 
-        #   + entry point: between open and low
-        #   + stop loss: above the high
-        # if bullish: 
-        #   + entry point: between close and high
-        #   + stop loss: below the low
-
-        for i in range(0, len(engulfing)):
-#            print(str(datetime.fromtimestamp(market_data["result"][engulfing[i]]["open_time"]))
-#            + " - open = " + str(market_data["result"][engulfing[i]]["open"])
-#            + " - close = " + str(market_data["result"][engulfing[i]]["close"])
-#            + " - high = " + str(market_data["result"][engulfing[i]]["high"])
-#            + " - low = " + str(market_data["result"][engulfing[i]]["low"])
-            logging.info(str(datetime.fromtimestamp(market_data["result"][engulfing[i]]["open_time"]))
-            + " - open = " + str(market_data["result"][engulfing[i]]["open"])
-            + " - close = " + str(market_data["result"][engulfing[i]]["close"])
-            + " - high = " + str(market_data["result"][engulfing[i]]["high"])
-            + " - low = " + str(market_data["result"][engulfing[i]]["low"])
-            )
+    
+    ma.market_analysis_export_engulfing_structure_from_main(argv)
 
     return 0
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, filename="logfile_" + str(date.today()), filemode="a+",
+    logging.basicConfig(level=logging.DEBUG, filename="./log/logfile_" + str(date.today()), filemode="a+",
                         format="%(asctime)-15s %(levelname)-8s %(message)s")
     logging.info("hello")
     main(sys.argv)
